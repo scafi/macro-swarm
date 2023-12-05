8805910557eecabf37f57363912a6c98edd39d1f
cric96 <gianluca.aguzzi@studio.unibo.it>
2023-12-05 15:40:15 +0000

    Deploying to gh-pages from @ scafi/macro-swarm@456779dcfac940de790e99ba436eb522d5e0003b 🚀

--- js/search.js
+++ js/search.js
@@ -45,7 +45,7 @@ function prepareIdxAndDocMap() {
     {
       "title": "Quick start",
       "url": "/macro-swarm/guide/quick.html",
-      "content": "MacroSwarm is based on the notion of computational fields, which are functions that map a position in space to a value. In particular, In this case each collective movement is expressed as a computational field, which is a function that maps a position in space to a velocity vector. These fields can be composed to create more complex behaviors. Thus, in this library, we collect several main basic behaviors that can be composed to create more complex behaviors. This structure is exemplified in the following figure: In the following, we describe the main function exposed by the library."
+      "content": "MacroSwarm is based on the notion of computational fields, which are functions that map a position in space to a value. In particular, In this case each collective movement is expressed as a computational field, which is a function that maps a position in space to a velocity vector. These fields can be composed to create more complex behaviors. Thus, in this library, we collect several main basic behaviors that can be composed to create more complex behaviors. This structure is exemplified in the following figure: In the following, we describe the main function exposed by the library. For more details, please refer to the API documentation."
     } ,      
   ];
 
